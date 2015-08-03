6c003a09748c454391d01bcc584960903fea213c
Stiliyan Sabchew <ssabchew@yahoo.com>
2015-08-03 18:05:25 +0300

    fix build

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
 
+VER=1.0
 RPMBUILD_DIR=$(rpm --eval %{_topdir})
 
 pushd "$(dirname $0)" > /dev/null
+tar  -pczf sty-etc-"${VER}".tar.gz sty-etc-"${VER}"
+
 RPM_DIR="${PWD}"
 popd > /dev/null
 
 cd "${RPMBUILD_DIR}"/SPECS
 cp "${RPM_DIR}"/*.spec .
+cp "${RPM_DIR}"/sty-etc-"${VER}".tar.gz ../SOURCES
 cd -
 
-spectool -g -R "${RPMBUILD_DIR}"/SPECS/sty-etc.spec
+#spectool -g -R "${RPMBUILD_DIR}"/SPECS/sty-etc.spec
 rpmbuild --clean -ba "${RPMBUILD_DIR}"/SPECS/sty-etc.spec
